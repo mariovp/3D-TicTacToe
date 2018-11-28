9333a174ee4779c55d0caac071dc30f8fa3ec374
Mario Valenzuela <mariovalpa@gmail.com>
2018-11-28 11:41:23 -0700

    Se simplifica función de evaluación, aun no se por qué funciona tan bien

--- src/com/valpa/player/EvaluationFunction.java
+++ src/com/valpa/player/EvaluationFunction.java
@@ -5,8 +5,6 @@ import com.valpa.board.Symbol;
 import com.valpa.board.WinningLinesPositions;
 
 import java.util.Arrays;
-import java.util.List;
-import java.util.stream.Collectors;
 
 public class EvaluationFunction {
 
@@ -22,50 +20,18 @@ public class EvaluationFunction {
 
         for (int[] winLine : winningLinesPositions.getWinningLinesPositionArray()) {
 
-            List<Integer> playerMovePositionsInLine = Arrays.stream(winLine).filter(pos -> {
-                Symbol symbol = board3D.getCubeCell(pos);
-                return symbol == aiPlayerSymbol || symbol == enemySymbol;
-            }).boxed().collect(Collectors.toList());
+            int aiPlayerSymbolCount = (int) Arrays.stream(winLine).filter(position -> board3D.getCubeCell(position) == aiPlayerSymbol).count();
+            int enemySymbolCount = (int) Arrays.stream(winLine).filter(position -> board3D.getCubeCell(position) == enemySymbol).count();
 
-            if (playerMovePositionsInLine.size() > 0) {
-                int aiPlayerSymbolCount = (int) playerMovePositionsInLine.stream().filter(pos -> board3D.getCubeCell(pos) == aiPlayerSymbol).count();
-                int enemySymbolCount = (int) playerMovePositionsInLine.stream().filter(pos -> board3D.getCubeCell(pos) == enemySymbol).count();
+            boolean hasAiPlayerPointsOnly = aiPlayerSymbolCount > 0 && enemySymbolCount == 0;
+            boolean hasEnemyPointsOnly = enemySymbolCount > 0 && aiPlayerSymbolCount == 0;
 
-                if (enemySymbolCount == 0) {
-                    points -= Math.pow(10, aiPlayerSymbolCount);
-                } else if (aiPlayerSymbolCount == 0) {
-                    points += Math.pow(11, enemySymbolCount);
-                }
-
-            }
+            if (hasAiPlayerPointsOnly)
+                points -= Math.pow(10, aiPlayerSymbolCount);
+            else if (hasEnemyPointsOnly)
+                points += Math.pow(11, enemySymbolCount);
         }
 
-        return points;
-    }
-
-    private int evaluate(Board3D board3D, Symbol playerSymbol) {
-        int points = 0;
-        for (int[] line : winningLinesPositions.getWinningLinesPositionArray()) {
-
-            int lineCoincidences = 0;
-            for (int pos : line) {
-
-                Symbol symbol = board3D.getCubeCell(pos);
-                if (symbol == playerSymbol)
-                    lineCoincidences++;
-            }
-
-            switch (lineCoincidences) {
-                case 1: points +=1;
-                    break;
-                case 2: points +=10;
-                    break;
-                case 3: points += 100;
-                    break;
-                case 4: points += 1000;
-                    break;
-            }
-        }
         return points;
     }
 
